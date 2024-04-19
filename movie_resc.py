cf8afa33cbd1908083368b7edda69d7774ac4beb
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-04-20 01:30:11 +1000

    adding url and toke

--- movie_resc.py
+++ movie_resc.py
@@ -1,7 +1,18 @@
 import pymongo
+from dotenv import load_dotenv
+import os
 
-client = pymongo.MongoClient("mongodb+srv://tonynhanvo:2ougHiUbBKAWkCDK@cluster0.yuaggbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+load_dotenv()
+
+mongo_password = os.getenv("MONGO_PASSWORD")
+client = pymongo.MongoClient(f"mongodb+srv://tonynhanvo:{mongo_password}@cluster0.yuaggbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.sample_mflix
 collection = db.movies
 
-print(collection.find().limit(5))
+hf_token = os.getenv("HF_TOKEN")
+model_id = os.getenv("MODEL_ID")
+embedding_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+
+items = collection.find().limit(5)
+for item in items:
+    print(item)
